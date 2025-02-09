7b08f7e5378276363eeb040647c2361d7382d121
pichulol96 <75361618+pichulol96@users.noreply.github.com>
2025-02-09 17:54:18 -0600

    pulgins sin cdn

--- panel.php
+++ panel.php
@@ -8,11 +8,11 @@ $ip = $objetoIP->obtenerIP();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="/wafleria/plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <script src="/wafleria/plugins/bootstrap/bootstrap.bundle.min.js"></script>
     <title>Panel Administrativo</title>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.all.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css">
+    <script src="/wafleria/plugins/sweetalert2/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="/wafleria/plugins/sweetalert2/sweetalert2.min.css">
     <style>
         .icon-delete{
             width: 15px;
